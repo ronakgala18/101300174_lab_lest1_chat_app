c335caccf4e2e6a50e32185e104b6df753bf7455
ronakgala18 <ronakgala18@gmail.com>
2023-02-07 10:14:42 -0500

    creating rooms

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ const socketio = require('socket.io')
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
+const formatMessage = require('./utils/messages')
 //SET static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
+const botName = " Chat App Bot"
 //run when client connects 
 io.on('connection', socket=>{
-    console.log("new Connection")
+    socket.on('joinRoom', ({ username, room }) => {
+
+        //welcome current user 
+        socket.emit('message', formatMessage(botName, "Welcome to chat app"))
+
+        //broadcast when user connects
+        socket.broadcast.emit('message', formatMessage(botName, "A user has joined the chat"));
+
+    })
+
+    
+    //listen for chat msg 
+    socket.on('chatMessage',(msg)=>{
+        io.emit('message',formatMessage('user', msg))
+    })
+
+    
+    //run when discconects
+    socket.on('disconnect',()=>{
+        io.emit('message',formatMessage(botName,"A user has left the chat"))
+    })
+
 })
 const PORT = 3000 || process.env.PORT;
 
